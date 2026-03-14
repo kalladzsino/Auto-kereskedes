1f2bfc3d6bb407e0ace4e7e5e4731c9ecbab04fb
TD GAMES <danieltoth706@gmail.com>
2026-03-14 13:15:25 +0100

    uj fooldal design
    a navbart majd kesobb megcsinalom rendesen reszponzivra

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ function includeHtml(name) {
         .then(response => response.text())
         .then(html => document.querySelector(name).innerHTML = html);
 }
+
+// amikor a felhasználó görgeti az oldaltt akkor rárakjuk a classt hogy 100%ra animálójon
+window.addEventListener("scroll", function() {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 10) {
+        navbar.classList.add("navbar-scrolled");
+    } else {
+        navbar.classList.remove("navbar-scrolled");
+    }
+});
