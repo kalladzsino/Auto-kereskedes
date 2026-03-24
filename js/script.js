3d7c1bd1b646d2d78c7f2f4d1a0ec0053c5f0e05
TTHDaniel <toth.daniel-2023@keri.mako.hu>
2026-03-24 09:57:32 +0100

    müködö kereses :)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 let autok = [];
 let AutokFilter = [];
 
+// html filter bemenetek
+let markaInput;
+let modellInput;
+let kivitelDropdown = ""; // a dropdownban kivalasztott érték fogja felulirni a setDropdownValue functionnal
+
+document.addEventListener("DOMContentLoaded", function() {
+    markaInput = document.getElementById("markaInput");
+    modellInput = document.getElementById("modellInput");
+});
+
+function setDropdownValue(value) {
+    kivitelDropdown = value;
+    dropdownSzoveg.textContent = value; // a dropdown gomb szoveget is atirjuk a kivalasztott ertekre
+    if (value === "") {
+        dropdownSzoveg.textContent = "Kérem Válasszon";
+    }
+}
+
 function includeHtml(name) {
     fetch(`./${name}.html`)
         .then(response => response.text())
@@ -27,7 +45,7 @@ async function JsonBetoltes() {
         // csak akkor generaljuk a kartyakat ha a vasarlas oldalon vagyunk,hogy ne legyen error
         if (document.body.id === 'vasarlas') {
             autok.forEach(item => {
-            CreateCard(item.marka, item.modell, item.kivitel, item.evjarat, item.ar, item.foto);
+            CreateCard(item.marka, item.modell, item.kivitel, item.evjarat, item.ar, item.foto, item.uzemanyag);
             });
         }
 
@@ -37,9 +55,7 @@ async function JsonBetoltes() {
 };
 JsonBetoltes()
 
-
-
-function CreateCard(marka, modell, kivitel, evjarat, ar, foto) {
+function CreateCard(marka, modell, kivitel, evjarat, ar, foto, uzemanyag) {
     let TalalatokDiv = document.getElementById("talalatok");
     const card = document.createElement(`div`);
 
@@ -49,7 +65,8 @@ function CreateCard(marka, modell, kivitel, evjarat, ar, foto) {
             <img src="${foto}" onerror="this.onerror=null; this.src='../assets/img/images/cars/default.png';" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${marka} ${modell}</h5>
-                <p class="card-text">${kivitel} | ${evjarat}</p>
+                <p>${kivitel} | ${evjarat} <br> ${uzemanyag}</p>
+
                 <p class="card-text card-text-mini"> ${numberWithSpaces(ar)} Ft</p>
             </div>
             <button class="btn btn-primary" type="button" aria-expanded="false" style="width: 100%;">
@@ -60,9 +77,41 @@ function CreateCard(marka, modell, kivitel, evjarat, ar, foto) {
     `;
     
     TalalatokDiv.appendChild(card);
-}
+};
 
 // szamok formazasa ezres elvalaszto spaceszel
 function numberWithSpaces(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-}
+};
+
+function Kereses() {
+    const marka = markaInput.value.toLowerCase();
+    const modell = modellInput.value.toLowerCase();
+    const kivitel = kivitelDropdown.toLowerCase();
+
+    // szűrés a bemenetek alapján
+    AutokFilter = autok.filter(auto => {
+        return (
+            (marka === "" || auto.marka.toLowerCase().includes(marka)) && //márkára szűrés (ha üres akkor minden találatot visszaad)
+            (modell === "" || auto.modell.toLowerCase().includes(modell)) && //modellre szűrés (ha üres akkor minden találatot visszaad)
+            (kivitel === "" || auto.kivitel.toLowerCase().includes(kivitel))
+        );
+    });
+
+    // regi kartyak torlese
+    const TalalatokDiv = document.getElementById("talalatok");
+    TalalatokDiv.innerHTML = "";
+
+    // új kártyák létrehozása
+    AutokFilter.forEach(item => {
+        CreateCard(
+            item.marka,
+            item.modell,
+            item.kivitel,
+            item.evjarat,
+            item.ar,
+            item.foto,
+            item.uzemanyag
+        );
+    });
+};
